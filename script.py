c758f783f86db746a3d41b6b6b3e0c703b101a9f
Oskamp <ralph.oskamp@de.bp.com>
2019-12-14 20:05:32 +0100

    Initial version

---
+++ script.py
@@ -0,0 +1,22 @@
+import math
+import os
+import sys
+import requests
+
+# print(sys.version)
+# print(sys.executable)
+
+
+# def greet(who_to_greet):
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
+
+
+# print(greet("World"))
+# print(greet("Ralph"))
+# r = requests.get("http://bp.com")
+
+# print(r.status_code)
+
+name = input("your name:")
+print("Helle ", name)
